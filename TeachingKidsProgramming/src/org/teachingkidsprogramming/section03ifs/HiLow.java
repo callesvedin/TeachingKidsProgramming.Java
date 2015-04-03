d04524b44e0544222028a0b938ee67c4e64a1094
Carl-Johan Svedin <carl-johan.svedin@infomaker.se>
2015-04-03 10:27:35 +0200

    Translated HiLow to swedish

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/HiLow.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/HiLow.java
@@ -4,18 +4,18 @@ public class HiLow
 {
   public static void main(String[] args)
   {
-    //    Choose a random number between 1 and 100 --#4.1 (fake!) & --#13
-    //    Do the following 8 times --#9.1
-    //          Ask the user for a guess --#1
-    //          If the guess is correct --#4
-    //               Play a bell --#2
-    //               Tell the user that they won the game  --#3
-    //               and exit --#10
-    //         Otherwise, if the guess is too high --#6
-    //               Tell the end user that it is too high --#5
-    //         Otherwise, if the guess is too low --#8
-    //               Tell the end user that it is too low --#7
-    //    Repeat --#9.2
-    //    After 8 incorrect guesses tell the user they've lost --#11
+    //    Välj ett tal mellan 1 och 100 -- #4.1 (fake!) & --#13
+    //    Gör följande 8 gånger --#9.1
+    //          Be användaren om en gissning --#1
+    //          Om användaren gissat rätt --#4
+    //               Spela upp ett klockljud --#2
+    //               Tala om för användaren att hen har vunnit --#3
+    //               Avsluta programmet --#10
+    //         Annars, om användaren gissat för högt --#6
+    //               Tala om för användaren att hen gissat för högt --#5
+    //         Annars, om gissningen är för låg --#8
+    //               Tala om för användaren att hen gissat för lågt --#7
+    //    Repetera --#9.2
+    //    Efter 8 felaktiga svar-Tala om för användaren att hen har förlorat --#11
   }
 }
