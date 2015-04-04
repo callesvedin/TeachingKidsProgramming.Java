00b837cce3694ee3b5b4e673a329333e548c69ab
Carl-Johan Svedin <carl-johan.svedin@infomaker.se>
2015-04-04 17:41:01 +0200

    PentagonCrazy translated to swedish

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
@@ -4,36 +4,36 @@ public class PentagonCrazy
 {
   public static void main(String[] args)
   {
-    //    Make the tortoise move as fast as possible --#3
-    //    createColorPalette (recipe below) --#8.1
+    //    Gör så att sköldpaddan rör sig så fort som möjligt --#3
+    //    Anropa skapaFärgpalett(createColorPalette) --#8.1
     //
-    //    ------------- Recipe for createColorPalette (HINT: Use PenColors)--#8.2
-    //    Add steel blue to the color wheel --#7
-    //    Add dark orchid to the color wheel --#11
-    //    Add dark slate blue to the color wheel --#12
-    //    Add teal to the color wheel --#13
-    //    Add indigo to the color wheel --#14
-    //    ------------- End of createColorPalette recipe --#8.3
+    //    ------------- Recept för createColorPalette (Ledtråd: Använd PenColors)--#8.2        
+    //    Lägg till stålblå (steel blue) till färghjulet --#7
+    //    Lägg till mörk orchid (dark orchid) till färghjulet --#11
+    //    Lägg till mörk matt blå (dark slate blue) till färghjulet--#12
+    //    Lägg till teal(?) (teal) till färghjulet --#13
+    //    Lägg till indigoblå (indigo) till färghjulet --#14
+    //    ------------- Slut på Recept för createColorPalette --#8.3
     //
-    //    drawPentagon (recipe below) --#10.1
+    //    Anropa rita pentagon receptet (drawPentagon) --#10.1
     //
-    //    ------------- Recipe for drawPentagon --#10.2
-    //    Do the following 200 times --#2.1
-    //        adjustPen (recipe below) --#9.1
+    //    ------------- Recept för rita pentagon (drawPentagon) --#10.2
+    //    Gör följande 200 gånger --#2.1
+    //        Anropa ändra pennan receptet (adjustPen) --#9.1
     //
-    //        ------------- Recipe for adjustPen --#9.2
-    //        Change the pen color of the line the tortoise draws to the next color on the color wheel --#6
-    //        Increase the tortoises pen width by 1 --#15                                              
-    //        If the tortoises pen width is greater than 4, then --#17
-    //            Reset it to 1 --#16
-    //        ------------- End of adjustPen recipe --#9.3 
+    //        ------------- Recept för ändra pennan (adjustPen) --#9.2
+    //        Byt färg på sköldpaddand penna till nästea färg i färghjulet --#6
+    //        Öka bredden på sköldpaddans penna med 1 --#15                                              
+    //        Om bredden på sköldpaddans penna är större än 4 så: --#17
+    //            Återställ pennans bredd till 1 --#16
+    //        ------------- Slut på recept för att ändra pennan (adjustPen) --#9.3 
     //
-    //        The current length of a side is the same as the number of the side you are about to draw ( 1st side = 1 pixel, 2nd side = 2 pixels, etc) --#4.2
-    //        Move the tortoise the length of a side --#4.1
-    //        Turn the tortoise 1/5th of 360 degrees --#1
-    //        Turn the tortoise 1 more degree --#5
-    //    Repeat --#2.2
-    //    ------------- End of drawPentagon recipe --#10.3
+    //        Den nuvarande längden på en sida är samma som numret på den sida du precis ska rita ( 1a sidan = 1 pixel, 2a sidan = 2 pixelar, osv) --#4.2    
+    //        Flytta sköldpaddan längden på en sida --#4.1
+    //        Sväng sköldpaddan 1/5 av 360 grader --#1
+    //        Sväng sköldpaddan 1 grad till--#5
+    //    Repetera --#2.2
+    //    ------------- Slut på rita pentagon receptet (drawPentagon) --#10.3
   }
   // Related Videos:
   // Sub recipe-> Method : http://youtu.be/C6fnqjceVcs
