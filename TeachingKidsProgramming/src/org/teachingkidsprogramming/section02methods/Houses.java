93402fe3d92fdda4e1d46da842ea711a2f5b0159
Carl-Johan Svedin <carl-johan.svedin@infomaker.se>
2015-04-04 16:26:49 +0200

    Ella after HiLow

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section02methods/Houses.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section02methods/Houses.java
@@ -1,27 +1,18 @@
 package org.teachingkidsprogramming.section02methods;
 
+import org.teachingextensions.logo.PenColors;
+import org.teachingextensions.logo.Tortoise;
+
 public class Houses
 {
   public static void main(String[] args)
   {
-    //   Flytta sköldpaddan så fort som möjligt --#11
-    //   Gör så att sköldpaddan startar 200pixlar in på X-axeln --#14
-    //   Sätt nuvarande höjd(height) till 40  --#1.2
-    //   Rita hus recept (drawHouse) --#9.1
-    //   ------------- Recept för rita hus (drawHouse) --#9.2
-    //       Byt färg på strecket sköldpaddan ritar till ljusgrå (lightGray) --#15
-    //      Flytta sköldpaddan höjden av ett hus --#1.1
-    //      Vänd sköldpaddan 90grader till höger--#2
-    //      Flytta sköldpaddan 30 pixlar --#3
-    //      Vänd sköldpaddan 90grader till höger --#4
-    //      Flytta sköldpaddan höjden av ett hus --#5
-    //      Vänd sköldpaddan 90 grader vänster --#6
-    //       Flytta sködpaddan 20 pixlar --#7
-    //       Vänd sköldpaddan 90 grader vänster --#8
-    //   ------------- Slut på recept för rita hus (drawHouse) --#9.3
-    //   Rita hus med höjden 120  --#10
-    //   Rita hus med höjden  90  --#12
-    //   Rita hus med höjden  20  --#13
+    Tortoise.setSpeed(10);
+    Tortoise.setX(200);
+    drawHouse(40);
+    drawHouse(120);
+    drawHouse(90);
+    drawHouse(20);
   }
   /*
    * När du är klar se filmen http://youtu.be/nQlsrnaMeuw
@@ -29,4 +20,16 @@ public class Houses
    * Andra relaterade filmer
    * Underrecept -> Metod : http://youtu.be/C6fnqjceVcs
    */
+  private static void drawHouse(int height)
+  {
+    Tortoise.setPenColor(PenColors.Grays.LightGray);
+    Tortoise.move(height);
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
 }
--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/HiLow.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/HiLow.java
@@ -1,21 +1,34 @@
 package org.teachingkidsprogramming.section03ifs;
 
+import org.teachingextensions.approvals.lite.util.NumberUtils;
+import org.teachingextensions.logo.utils.Sounds;
+import org.teachingextensions.windows.MessageBox;
+
 public class HiLow
 {
   public static void main(String[] args)
   {
-    //    Välj ett tal mellan 1 och 100 -- #4.1 (fake!) & --#13
-    //    Gör följande 8 gånger --#9.1
-    //          Be användaren om en gissning --#1
-    //          Om användaren gissat rätt --#4
-    //               Spela upp ett klockljud --#2
-    //               Tala om för användaren att hen har vunnit --#3
-    //               Avsluta programmet --#10
-    //         Annars, om användaren gissat för högt --#6
-    //               Tala om för användaren att hen gissat för högt --#5
-    //         Annars, om gissningen är för låg --#8
-    //               Tala om för användaren att hen gissat för lågt --#7
-    //    Repetera --#9.2
-    //    Efter 8 felaktiga svar-Tala om för användaren att hen har förlorat --#11
+    int min = 1;
+    int max = 100;
+    int svar = NumberUtils.getRandomInt(min, max);
+    for (int i = 1; i <= 8; i++)
+    {
+      int number = MessageBox.askForNumericalInput("Säg ett nummer mellan " + min + " och " + max);
+      if (number == svar)
+      {
+        Sounds.playBeep();
+        MessageBox.showMessage("Du har vunit");
+        System.exit(0);
+      }
+      else if (number > svar)
+      {
+        MessageBox.showMessage("För högt");
+      }
+      else if (number < svar)
+      {
+        MessageBox.showMessage("För lågt");
+      }
+    }
+    MessageBox.showMessage("Du har förlorat. Svaret var " + svar);
   }
 }
