a5e1ae4ef7671b7f9861b6a121bb105bf39a250e
Carl-Johan Svedin <carl-johan.svedin@infomaker.se>
2016-03-17 16:37:54 +0100

    ChooseYourOwnAdventure WIP

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/ChooseYourOwnAdventure.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/ChooseYourOwnAdventure.java
@@ -1,28 +1,26 @@
 package org.teachingkidsprogramming.section03ifs;
 
+import java.awt.Color;
+
+import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.windows.MessageBox;
 
 public class ChooseYourOwnAdventure {
     public static void main(String[] args) {
 	startStory();
+
     }
 
     private static void startStory() {
 	MessageBox.showMessage("En morgon vaknade sköldpaddan upp i en dröm.");
 	//
-	// Anropa animera börja historian receptet (animateStartStory) --#38.1
+
+	animateStartStory();
 	//
 	// ------------- Recept för att animera börja historien
-	// (animateStartStory) --#38.2
-	// Visa sköldpaddan --#37
-	// Nuvaranande färg är svart --#39.2
-	// Gör följande 25 gånger --#41.1
-	// Byt bakgrund till nuvarande penfärg --#39.1
-	// Gör nuvarande penfärg ljusare (lighter) --#42
-	// Vänta i 100 millisekunder --#40
-	// Repetera --#41.2
-	// ------------- Slut på animateStartStory recept --#38.3
-	//
+
+	Tortoise.show();
+
 	String svar = MessageBox
 		.askForTextInput("Vill du 'vakna' eller 'utforska' drömmen?");
 	if (svar.equalsIgnoreCase("vakna")) {
@@ -32,6 +30,17 @@ public class ChooseYourOwnAdventure {
 	} else {
 	    badAnswer();
 	}
+    }
+
+    private static void animateStartStory() {
+	Tortoise.setPenColor(Color.BLACK);
+	// Nuvaranande färg är svart --#39.2
+	// Gör följande 25 gånger --#41.1
+	// Byt bakgrund till nuvarande penfärg --#39.1
+
+	// Gör nuvarande penfärg ljusare (lighter) --#42
+	// Vänta i 100 millisekunder --#40
+	// Repetera --#41.2
     }
 
     private static void badAnswer() {
@@ -80,24 +89,27 @@ public class ChooseYourOwnAdventure {
     private static void pourIntoToilet() {
 	MessageBox
 		.showMessage("När du häller kleten i toaletten försvinner det undan gurglar och exploderar över dig med radioaktivt avfall.");
-	// Fråga användaren
-	// "Vill du träna för att bli en NINJA? 'Ja' eller 'Givet'?" --#
-	// Om svaret är "yes" --#32
-	// Anropa receptet ninjasköldpadda (ninjaTortoise) --#31.1
+
+	String svar = MessageBox
+		.askForTextInput("Vill du träna för att bli en NINJA? 'Ja' eller 'Givet'?");
+
+	if (svar.equalsIgnoreCase("Ja"))
+
+	    ninjaTotoise();
+	else if (svar.equalsIgnoreCase("Givet")) {
+	    ninjaTotoise();
+	}
+
+	else {
+	    badAnswer();
+	}
 	//
-	// ------------- Recept för ninjasköldpadda (ninjaTortoise) --#31.2
-	// Säg till användaren
-	// "Häftigt!  Du lever resten av ditt liv genom att bekämpa brott och äta pizza!"
-	// --#30
-	// ------------- Slut på ninjasköldpadda-receptet (ninjaTortoise)
-	// --#31.3
-	//
-	// Annars, om svaret är "Givet" --#34
-	// Anropa ninjasköldpadda receptet (ninjaTortoise) --#33
-	//
-	// Annars, om svaret är något annat --#36
-	// Anropa fel svar receptet (badAnswer) --#35
-	//
+    }
+
+    private static void ninjaTotoise() {
+	MessageBox
+		.showMessage("Häftigt! Du lever resten av dit liv genom att bekämpa brott och äta pizza!");
+
     }
 
     private static void wakeUp() {
